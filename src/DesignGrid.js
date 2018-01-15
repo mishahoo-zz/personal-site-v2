d79402ff55bd59e5109beb03dd6cebf9e222b912
Misha Hoo <hoomisha@gmail.com>
2018-01-14 22:31:07 -0600

    Display DesignCards with flexbox.

--- src/DesignGrid.js
+++ src/DesignGrid.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import DesignCard from './DesignCard';
 import data from './data';
+import './DesignGrid.css';
 
 const DesignGrid = () => (
-  <div className="container col-md-8">{data.map(card => <DesignCard key={card.title} card={card} />)}</div>
+  <div id="design-grid" className="container">
+    {data.map(card => <DesignCard key={card.title} card={card} />)}
+  </div>
 );
 
 export default DesignGrid;
