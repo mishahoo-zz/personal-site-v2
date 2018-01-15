30ccf502b613d02650c69d915bbadc8c1c26c567
Misha Hoo <hoomisha@gmail.com>
2018-01-14 21:12:10 -0600

    Add DesignGrid component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import About from './About';
+import DesignGrid from './DesignGrid';
 import Footer from './Footer';
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <About />
+        <DesignGrid />
         <Footer />
       </div>
     );
---
+++ src/DesignGrid.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DesignGrid = () => (
+  <div className="container">
+    <p>Design Grid</p>
+  </div>
+);
+
+export default DesignGrid;
