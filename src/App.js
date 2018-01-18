4586416f05112ad398c77d25fc07ecd71a990fb6
Misha Hoo <hoomisha@gmail.com>
2018-01-17 23:32:08 -0600

    Add 404 route and import switch to deal with it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './Navigation';
 import Landing from './Landing';
 import Project from './Project';
 import Footer from './Footer';
 import './App.css';
+
+const fourOhFour = () => <h1>404</h1>;
 
 class App extends Component {
   render() {
@@ -12,8 +14,11 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navigation />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/project" component={Project} />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/project" component={Project} />
+            <Route component={fourOhFour} />
+          </Switch>
           <Footer />
         </div>
       </BrowserRouter>
